d20e55b33c902f940e627d0638d64dd233b9f5f9
driversti <driversti@gmail.com>
2021-09-23 18:59:29 +0200

    init commit

---
+++ src/main/java/dev/seniorjava/datefy/DatefyApplication.java
@@ -0,0 +1,13 @@
+package dev.seniorjava.datefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatefyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DatefyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/seniorjava/datefy/DatefyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.seniorjava.datefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatefyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
